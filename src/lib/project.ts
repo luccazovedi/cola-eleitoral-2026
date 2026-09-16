46393786146e0fde2a33531429d0c6153bda3163
Lucca Zovedi <luccazovedi@gmail.com>
2026-09-16 00:46:47 -0300

    feat: adiciona constantes do projeto

---
+++ src/lib/project.ts
@@ -0,0 +1,29 @@
+export const officialSources = [
+  {
+    label: "Portal de Dados Abertos do TSE",
+    href: "https://dadosabertos.tse.jus.br/",
+  },
+  {
+    label: "Candidatos - 2026",
+    href: "https://dadosabertos.tse.jus.br/dataset/candidatos-2026",
+  },
+  {
+    label: "Divulgação de Candidaturas e Contas Eleitorais",
+    href: "https://divulgacandcontas.tse.jus.br/divulga/#/",
+  },
+] as const;
+
+export const electionFlow = [
+  "Deputado federal",
+  "Deputado estadual ou distrital",
+  "Senador - 1a escolha",
+  "Senador - 2a escolha",
+  "Governador",
+  "Presidente",
+] as const;
+
+export const privacyPrinciples = [
+  "Sem login obrigatorio",
+  "Escolhas salvas apenas no dispositivo",
+  "Sem envio de candidato ou partido para analytics",
+] as const;
